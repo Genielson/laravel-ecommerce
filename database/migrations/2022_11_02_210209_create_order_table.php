aa20534ed261ccb6c1b50e47c427e31c75bb004a
Genielson <ge.nielson@hotmail.com>
2022-11-02 18:10:38 -0300

    Adicionando model Order

---
+++ database/migrations/2022_11_02_210209_create_order_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_cart');
+            $table->unsignedFloat('id_user');
+            $table->unsignedTinyInteger('total_value');
+            $table->timestamps();
+        });
+
+        Schema::table('orders', function(Blueprint $table){
+            $table->foreign('id_cart')->references('id')->on('carts')->onDelete('cascade');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
