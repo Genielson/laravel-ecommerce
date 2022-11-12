82919106103fd816b98362881982f255218e3c73
Genielson <ge.nielson@hotmail.com>
2022-11-12 17:31:05 -0300

    Adicionando view de lista de desejos

--- resources/views/account/wish-list.blade.php
+++ resources/views/account/wish-list.blade.php
@@ -5,17 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Meus Pedidos  </div>
+                <div class="card-header">Meus Desejos </div>
+
 
                 <table class="table">
                     <thead>
                       <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Numero do pedido</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Imagem</th>
-                        <th scope="col">Data do pedido</th>
-                        <th scope="col">Situação </th>
+                        <th scope="col">Nome </th>
+                        <th scope="col">Categoria </th>
+                        <th scope="col"> Imagem </th>
 
                       </tr>
                     </thead>
@@ -24,11 +23,9 @@
                       <tr>
                         <th scope="row"></th>
                         <td></td>
-                        <td>1000</td>
-                        <td>Smartphone Samsung Galaxy S10 </td>
-                        <td><img src="{{ asset('img/algumacoisa.png') }}"></td>
-                        <td>23-10-2022</td>
-                        <td> Em separação </td>
+                        <td> Smartphone Samsung Galaxy S10 </td>
+                        <td> Eletronicos </td>
+                        <td> <img width="50px;" height="50px;" src="{{ asset('img/algumacoisa.png') }}"> </td>
                       </tr>
 
 
