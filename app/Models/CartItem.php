c8dfe0c06978ab0e334733b60c3407adff90581c
Genielson <ge.nielson@hotmail.com>
2022-11-15 10:25:56 -0300

    Adicionando funcionalidade no model Cart para retorno de todos os itens do atual carrinho

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class CartItem extends Model
 {
@@ -14,12 +15,13 @@ class CartItem extends Model
 
 
     public function addingOrIncrementItemCart($idCart,$idProduct,$quantity = null){
-        $itens = DB::table('cart_itens')->where('id',$idCart)
+        $itens = DB::table('cart_itens')->where('cart_id',$idCart)
         ->where('id_product',$idProduct)->get()->toArray();
+
         if(count($itens) >= 1){
-         DB::table('cart_itens')->where('id',$idCart)
-        ->where('id_product',$idProduct)->where('cart_id', $idCart)
-        ->update(['quantity' => $itens[0]->quantity + $quantity ]);
+            DB::table('cart_itens')->where('id',$itens[0]->id)
+            ->where('id_product',$idProduct)->where('cart_id', $idCart)
+            ->update(['quantity' => $itens[0]->quantity + $quantity ]);
         }else{
             DB::table('cart_itens')->insert([
                 'cart_id' => $idCart,
@@ -28,5 +30,14 @@ class CartItem extends Model
             ]);
         }
         return true;
+    }
+
+    public function getAllItensLastCart(){
+
+        $user = Auth::user();
+        $cart = Cart::where('status','')->where('id_user',$user->id)->get();
+        $itens = DB::table('cart_itens')->where('cart_id',$cart->id)->get()->toArray();
+        return $itens;
+
     }
 }
